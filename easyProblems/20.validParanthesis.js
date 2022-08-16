104f0a3522122badbff85be07ce81b42dade9970
Pantelis <pantpap@gmail.com>
2022-08-16 20:53:02 +0300

    20.validParanthesis

---
+++ easyProblems/20.validParanthesis.js
@@ -0,0 +1,15 @@
+function isValid(s){
+    let stack = [];
+    let pairs = {"(": ")", "{": "}", "[": "]"};
+
+    for(let i = 0; i < s.length; i++){
+        let char = s[i];
+
+        if(pairs[char]){
+            stack.push(char);
+        } else if(pairs[stack.pop()] !== char){
+            return false;
+        }
+    }
+    return stack.length === 0;
+}
